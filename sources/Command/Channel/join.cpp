a1ead3c571afb188aa26e3b396fccbd9f7f65e11
genouf <gabriel.enouf@yahoo.fr>
2023-02-12 21:57:10 +0100

    [FIX] join and leave (need refacto)

--- sources/Command/Channel/join.cpp
+++ sources/Command/Channel/join.cpp
@@ -5,23 +5,22 @@ void	Server::send_info_join(Channel &channel, std::string title, User &user)
 	std::string AllUsers;
 
 	std::cout << "User added to channel " << title << std::endl;
+	channel.addUser(&user);
 	std::map<int, User*> users = channel.getUsers();
 	for (std::map<int, User*>::iterator it = users.begin(); it != users.end(); it++)
-	{
 		AllUsers.append(it->second->getNick() + " ");
-		this->send_client(":" + user.getNick() + " JOIN " + title, it->second->getFd());
-	}
 	if (!channel.getTopic().empty())
 	{
 		std::string msg = ":127.0.0.1 332 " + user.getNick() + " " + title + " :" + channel.getTopic();
 		this->send_client(msg, user.getFd());
 	}
-	std::string msg = ":127.0.0.1 353 " + user.getNick() + " = " + title + " :";
-	msg.append(user.getNick() + " " + AllUsers);
-	channel.addUser(&user);
+	std::string msg = ":" + user.getNick() + "!" + user.getUsername() + "@127.0.0.1 353 " + user.getNick() + " = " + title + " :";
+	msg.append(AllUsers);
 	this->send_client(msg, user.getFd());
-	msg = ":127.0.0.1 366 " + user.getNick() + " " + title + " :End of NAMES list";
+	msg = ":" + user.getNick() + "!" + user.getUsername() + "@127.0.0.1 366 " + user.getNick() + " " + title + " :End of NAMES list";
 	this->send_client(msg, user.getFd());
+	for (std::map<int, User*>::iterator it = users.begin(); it != users.end(); it++)
+		this->send_client(":" + user.getNick() + "!" + user.getUsername() + "@127.0.0.1 JOIN " + title, it->second->getFd());
 }
 
 int		Server::cmd_join(std::vector<std::string> params, User &user)
